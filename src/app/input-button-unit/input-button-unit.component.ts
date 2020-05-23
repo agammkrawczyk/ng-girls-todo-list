211e5875b9746c261c72d5caaa3abe4a85ba686b
akrawczyk <agammkrawczyk@gmail.com>
2020-05-23 13:20:40 +0200

    first commit

--- src/app/input-button-unit/input-button-unit.component.ts
+++ src/app/input-button-unit/input-button-unit.component.ts
@@ -6,7 +6,7 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./input-button-unit.component.css']
 })
 export class InputButtonUnitComponent implements OnInit {
-  title = 'Hello Ng Grils';
+  title = 'Hello Ng Grils vikings ';
 
   constructor() {
   }
